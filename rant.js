57848d5901ffb68ec6434c65d33c99efdc07e201
eye-opener-tenzin <tmejay.tm@gmail.com>
2020-10-24 12:29:40 -0700

    add rant's html and js file

---
+++ rant.js
@@ -0,0 +1,9 @@
+// DEFINE YOUR FUNCTION:
+function rant(message) {
+    let newMsg = message.toUpperCase();
+   console.log(newMsg)
+   console.log(newMsg)
+   console.log(newMsg)
+  
+}
+rant('woop')
